72b74f04d5d10fdfda5455f696bffde4d6fb60a4
zuhui_he <eriche@apple.com>
2021-05-14 15:40:21 -0700

    add src

---
+++ src/Demo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const Demo = () => {
+  return <div>Hello World</div>;
+};
+
+export const Demo2 = () => {
+  return <div>react-npm-lib-template</div>;
+};
+
+export default Demo;
+
