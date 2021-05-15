cc946c9b2fbcb51da11005c9ba5e0d6c0036235a
zuhui_he <eriche@apple.com>
2021-05-14 18:11:33 -0700

    mod

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.css";
+import style from "./styles.module.css"
 
 const Demo = () => {
   return <div>Hello World</div>;
 };
 
 export const Demo2 = () => {
-  return <div>react-npm-lib-template</div>;
+  return <div className={style.title}>react-npm-lib-template</div>;
 };
 
 export default Demo;
